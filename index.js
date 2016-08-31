e487f9c829a3b6fe803b6b7ae973e531a1185039
Isaac Mast <isaac@pedago.com>
2016-08-31 13:04:13 -0400

    chore(fix formatting): fixed formatting for initial commit after forking

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-var _ = require('lodash')
-var Parser = require('pdf3json')
+var _ = require('lodash');
+var Parser = require('pdf3json');
 
 //clear the pdf logger
-require('util')._logN = function() { }
+require('util')._logN = function() {};
 
 //given a path to a pdf
 //turn it into a json structure
 module.exports = function(path, cb) {
-  var parser = new Parser()
-  parser.on('pdfParser_dataReady', function(result) {
+    var parser = new Parser();
+    parser.on('pdfParser_dataReady', function(result) {
 
-    var text = []
+        var text = [];
 
-    //get text on a particular page
-    result.data.Pages.forEach(function(page) {
-      var chunks = _(page.Texts).map('R').flatten().map('T').map(decodeURIComponent).value()
-      text = text.concat(chunks)
-    })
+        //get text on a particular page
+        result.data.Pages.forEach(function(page) {
+            var chunks = _(page.Texts).map('R').flatten().map('T').map(decodeURIComponent).value();
+            text = text.concat(chunks);
+        });
 
-    parser.destroy()
+        parser.destroy();
 
-    setImmediate(function() {
-      cb(null, text)
-    })
-  })
+        setImmediate(function() {
+            cb(null, text);
+        });
+    });
 
-  parser.on('pdfParser_dataError', function(err) {
-    parser.destroy()
-    cb(err)
-  })
-  if(path instanceof Buffer) {
-    return parser.parseBuffer(path)
-  }
-  parser.loadPDF(path)
-}
+    parser.on('pdfParser_dataError', function(err) {
+        parser.destroy();
+        cb(err);
+    });
+    if (path instanceof Buffer) {
+        return parser.parseBuffer(path);
+    }
+    parser.loadPDF(path);
+};
